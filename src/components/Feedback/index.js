b32d3056257b004cd01da5ae52e8064c0cb7d0bd
sai <saisreenath.mandaloju@gmail.com>
2024-12-21 12:46:53 +0000

    added Default export Feedback

--- src/components/Feedback/index.js
+++ src/components/Feedback/index.js
@@ -1,34 +1,33 @@
 import React from 'react'
 
-const Emoji = (emoji) => (
-    <div className='emoji-container'>
-        <img src={emoji.imageUrl} alt={emoji.name} />
-        <p className='emoji-title'>{emoji.name}</p>
-    </div>
+const Emoji = emoji => (
+  <div className="emoji-container">
+    <img src={emoji.imageUrl} alt={emoji.name} />
+    <p className="emoji-title">{emoji.name}</p>
+  </div>
 )
 
-
-const EmojiCard = (emojis) => (
-    <div className='container'>
-        <h1 className='heading'>
-            How satisfied are you with our customer support
-            performance? 
-        </h1>
-        {emojis.map(
-            each => (<Emoji emoji={each} />)
-        )}
-    </div>
+const EmojiCard = emojis => (
+  <div className="container">
+    <h1 className="heading">
+      How satisfied are you with our customer support performance?
+    </h1>
+    {emojis.map(each => (
+      <Emoji emoji={each} />
+    ))}
+  </div>
 )
 
-
-export const Feedback = (props) => {
-    const {emojis} = props
+export const Feedback = props => {
+  const {emojis} = props
 
   return (
     <div className="feedback-app">
-        <div className='card'>
-            <EmojiCard emojis = {emojis} />
-        </div>
+      <div className="card">
+        <EmojiCard emojis={emojis} />
+      </div>
     </div>
   )
 }
+
+export default Feedback
