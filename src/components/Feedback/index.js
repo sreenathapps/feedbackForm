dbc2bd41cd4e8052d614c06f79960201c485e909
sai <saisreenath.mandaloju@gmail.com>
2024-12-22 05:43:46 +0000

    final commit

--- src/components/Feedback/index.js
+++ src/components/Feedback/index.js
@@ -1,17 +1,35 @@
-import React from 'react'
+import {Component} from 'react'
+import './index.css'
 
-const Emoji = emoji => {
+const Emoji = props => {
+  const {emoji, onclick} = props
   const {name, imageUrl} = emoji
   return (
     <li className="emoji-container">
-      <img src={imageUrl} alt={name} />
+      <button type="button" onClick={onclick} className="button">
+        <img className="emoji" src={imageUrl} alt={name} />
+      </button>
       <p className="emoji-title">{name}</p>
     </li>
   )
 }
 
+const Thankyou = props => {
+  const {imageUrl} = props
+
+  return (
+    <div className="thank-you">
+      <img className="heart" src={imageUrl} alt="love emoji" />
+      <h1 className="heading">Thank You!</h1>
+      <p className="paragraph">
+        We will use your feedback to improve our customer support
+      </p>
+    </div>
+  )
+}
+
 const EmojiCard = props => {
-  const {resources} = props
+  const {onclick, resources} = props
 
   const {emojis} = resources
   console.log(emojis)
@@ -20,25 +38,43 @@ const EmojiCard = props => {
       <h1 className="heading">
         How satisfied are you with our customer support performance?
       </h1>
-      <ul>
+      <ul className="emojis-container">
         {emojis.map(each => (
-          <Emoji key={each.id} emoji={each} />
+          <Emoji onclick={onclick} key={each.id} emoji={each} />
         ))}
       </ul>
     </div>
   )
 }
 
-export const Feedback = props => {
-  const {resources} = props
+class Feedback extends Component {
+  state = {
+    currentPage: 1,
+  }
 
-  return (
-    <div className="feedback-app">
-      <div className="card">
-        <EmojiCard resources={resources} />
+  onClickEmoji = () => {
+    this.setState({
+      currentPage: 2,
+    })
+  }
+
+  render() {
+    const {resources} = this.props
+    const {currentPage} = this.state
+    const {loveEmojiUrl} = resources
+
+    return (
+      <div className="feedback-app">
+        <div className="card">
+          {currentPage === 1 ? (
+            <EmojiCard onclick={this.onClickEmoji} resources={resources} />
+          ) : (
+            <Thankyou imageUrl={loveEmojiUrl} />
+          )}
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Feedback
